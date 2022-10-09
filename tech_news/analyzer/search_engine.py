39e5dd2eecc5f2836eecfeb26939314da49861ce
Matheus <matheuscarretta@gmail.com>
2022-10-09 18:30:07 -0300

    feat: implementa busca por titulo

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
+# based on: https://www.mongodb.com/community/forums/t/case-insensitive-search-with-regex/120598
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = search_news({"title": {"$regex": title, "$options": "i"}})
+    news_by_title = []
+
+    for content in news:
+        news_by_title.append((content['title'], content['url']))
+
+    return news_by_title
 
 
 # Requisito 7
