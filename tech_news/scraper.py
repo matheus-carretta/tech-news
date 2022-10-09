dfc5afe5de804dcc309aef831f4837b271309613
Matheus <matheuscarretta@gmail.com>
2022-10-09 14:40:22 -0300

    feat: implementa a funcao scrape_novidades

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import time
 import requests
+from parsel import Selector
 
 
 # Requisito 1
@@ -17,7 +18,9 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    news_url = selector.css('.entry-header h2 a::attr(href)').getall()
+    return news_url
 
 
 # Requisito 3
